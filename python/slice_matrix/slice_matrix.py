56bbffbfacf8cc6ebab569548b73c4a12a2c94e0
Ines <inesucrvenom@gmail.com>
2020-08-04 16:09:04 +0200

    reformat file pep8

--- python/slice_matrix/slice_matrix.py
+++ python/slice_matrix/slice_matrix.py
@@ -1,9 +1,11 @@
 debug = 0
 
+
 def show_matrix(mat):
     result = '\n'.join([''.join(['{:3}'.format(item) for item in row])
-      for row in mat])
+                        for row in mat])
     print(result, '\n')
+
 
 DELTAS = [
     [0, 1, 2, 3, 4, 5, 6, 7],
@@ -15,11 +17,12 @@ DELTAS = [
     [6, 7, 4, 5, 2, 3, 0, 1],
     [7, 6, 5, 4, 3, 2, 1, 0]]
 
-# todo
+
 # save already computed sums, defined by largest item in matrix (bottom right in 8x8)
-prev = {} # key: (a + 7 - loss) % time
-prev_part = {} # call by ( min(r,c), max(r,c), (a+7-loss) % time ) TUPLE!!!
+prev = {}  # key: (a + 7 - loss) % time
+prev_part = {}  # call by ( min(r,c), max(r,c), (a+7-loss) % time ) TUPLE!!!
 MODULO = 0
+
 
 def initialise(t):
     global MODULO, prev, prev_part
@@ -33,8 +36,9 @@ def mod(num):
     global MODULO
     return num % MODULO
 
+
 def sum_submatrix(a, c, r, loss):
-    '''
+    """
     returns sum of elements of a submatrix of c columns and r rows
     c, r <= 8
 
@@ -50,12 +54,13 @@ def sum_submatrix(a, c, r, loss):
     a+5  a+4  a+7  a+6  a+1  a    a+3  a+2
     a+6  a+7  a+4  a+5  a+2  a+3  a    a+1
     a+7  a+6  a+5  a+4  a+3  a+2  a+1  a
-    '''
+    """
+
     global prev
     if not ((0 < c <= 8) and (0 < r <= 8)):
         raise ValueError("Row or col values are out of range")
 
-    biggest = (a+7) - loss # bottom right element
+    biggest = (a + 7) - loss  # bottom right element
 
     # we have null-matrix if smallest and highest number are 0
     if biggest <= 0:
@@ -64,20 +69,20 @@ def sum_submatrix(a, c, r, loss):
     # matrix with only 1s on reverse diagonal
     if biggest == 1:
         # top part, all 0
-        if (r < 8-c) or (c < 8-r):
+        if (r < 8 - c) or (c < 8 - r):
             return 0
         else:
-            return min(r,c) # has only min(r,c) 1s
+            return min(r, c)  # has only min(r,c) 1s
 
     # biggest > 1
     biggest_mod = mod(biggest)
 
     if 8 == r == c:
-        check = prev.get(biggest_mod) # for 8x8 matrix
-    elif r < c: # call for smaller, it's symmetrical
-        check = prev_part.get((r, c, biggest_mod)) # don't forget it's a tuple!
+        check = prev.get(biggest_mod)  # for 8x8 matrix
+    elif r < c:  # call for smaller, it's symmetrical
+        check = prev_part.get((r, c, biggest_mod))  # don't forget it's a tuple!
     else:
-        check = prev_part.get((c, r, biggest_mod)) # tuple!
+        check = prev_part.get((c, r, biggest_mod))  # tuple!
 
     if check is not None:
         return check
@@ -95,9 +100,9 @@ def sum_submatrix(a, c, r, loss):
     if 8 == r == c:
         prev[biggest_mod] = sum_a
     elif r < c:
-        prev_part[(r,c, biggest_mod)] = sum_a
+        prev_part[(r, c, biggest_mod)] = sum_a
     else:
-        prev_part[(c,r, biggest_mod)] = sum_a
+        prev_part[(c, r, biggest_mod)] = sum_a
 
     return sum_a
 
@@ -181,10 +186,10 @@ def sum_split_table(c, r, loss):
     return mod(total_sum)
 
 
-def elder_age(m,n,l,t):
+def elder_age(m, n, l, t):
     initialise(t)
     if debug:
-        print('---\n',m, n, l, t)
+        print('---\n', m, n, l, t)
     if debug == 2:
         show_matrix(DELTAS)
     return sum_split_table(m, n, l)
